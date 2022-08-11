5a842a9a40730144a95243d9583bf17da532639e
aditiani1 <ani25singh@gmail.com>
2022-08-11 12:38:48 +0530

    added cards

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const xhr = new XMLHttpRequest()
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos', true);
-xhr.onload= function(){
+xhr.onload = () => {
     const response = JSON.parse(xhr.responseText);
-    console.log(response)
+    // console.log(response)
 
+    let template = '';
+    for (let i=0; i<response.length; i++) {
+        template += `
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${response[i].thumbnailUrl}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${response[i].title}</h5>
+            </div>
+        </div>`;
+    }
+
+    document.querySelector("#photo-container").innerHTML = template
 }
 //4. request sent
 xhr.send();
